ccde4ec6fd3964f3112f92683c2193fdf7403dc0
Tess Monical <tess.monical@gmail.com>
2017-10-31 16:31:54 -0500

    added dialog and ipcMain requires

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const electron = require('electron')
+const electron = require('electron');
+const { dialog, ipcMain } = require('electron');
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.

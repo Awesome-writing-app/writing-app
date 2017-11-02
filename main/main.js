d8a3d738dbc63aa935a0358a4a23fb464ddf83bb
Tess Monical <tess.monical@gmail.com>
2017-11-02 10:05:20 -0500

    some file reorganization and added some communication with the render to main.js

--- main/main.js
+++ main/main.js
@@ -59,3 +59,9 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+//when the render process sends 'open-file', open a file-open dialog
+ipcMain.on('open-file', function() {
+  dialog.showOpenDialog(mainWindow, {properties: ['openFile', 'openDirectory', 'multiSelections']})
+})
